98a25767005d29db99bef2205ab5da295638c194
colowill <willminor64@gmail.com>
2024-11-06 15:46:17 -0500

    html styling in generator.js

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -9,18 +9,25 @@ function Generator() {
      */
     <div className="flex flex-col">
         <div className="flex flex-row justify-between mx-5 my-4 items-center">
-            <button className="border-2 border-solid border-blue-400 text-black text-xl">
+            <button className="border-2 border-solid border-green-300 text-black text-xl font-bold
+            rounded-md px-3 py-2 hover:bg-green-300">
                 Generate
             </button>
-            <h2 className="text-2xl font-bold">
+            <h2 className="text-2xl font-bold justify-between mx-1 my-1" style={{marginRight: '60px' }}>
                 LinkedIn Cover Letter Generator
             </h2>
-            <button classname="border mr-[1px] p-2 border-solid border-gray-600 rounded-[100%]">
-
+            <button className="border-2 px- mr-[3px] border-solid border-black rounded-full hover:bg-gray-200 
+             w-12 h-12 items-center justify-center text-3xl hover:font-bold">
+                i
             </button>
-            
         </div>
-
+        <div className="flex mx-5">
+            <textarea
+                rows={12}
+                className="w-full"
+                placeholder="Cover Letter"
+            />
+        </div>
     </div>
   )
 }
